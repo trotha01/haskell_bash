95ab2e9ca516f4aec8648070d25e844463c13714
Trevor Rothaus <trevor.rothaus@sendgrid.com>
2015-02-11 17:31:37 -0800

    Updated cat explanation

--- cat/cat.hs
+++ cat/cat.hs
@@ -18,9 +18,19 @@ maybeCat = getArgs >>= \files -> maybe (interact id) (\_ -> mapM readFile files 
 -- The first is the default value to use (if Maybe is Nothing)
 -- The second is the function to apply to 'a', in Just a
 -- The third is the Maybe value (either Just a or Nothing)
+-- Examples:
+--  > maybe 1 (\_ -> 2) (Just "a")
+--  2
+--  > maybe 1 (\_ -> 2) (Nothing)
+--  1
 
 -- listToMaybe changes a list into either Nothing, if the list is empty
 --      or Just a, where a is the first item in the list
+-- Examples:
+--  > listToMaybe []
+--  Nothing
+--  > listToMaybe ["hello","world"]
+--  Just "hello"
 
 -- interact takes a function (String -> String), takes standard input, applies the function to it,
 --  and returns the result to standard output
@@ -28,19 +38,20 @@ maybeCat = getArgs >>= \files -> maybe (interact id) (\_ -> mapM readFile files 
 
 -- "interact id" will take standard input and give it back as standard output
 
--- readFile simple takes a filename and reads it, returning an IO String
--- putStr takes in a string and returns IO ()
+-- readFile takes a filename and reads it to a string, returning an IO String
+-- putStr takes in a string, prints it to standard out and returns IO ()
 
 -- \a -> ... Is just a lambda function.
 
--- mapM makes more sense with it's signature, Monad m => (a -> m b) -> [a] -> m [b]
---      It takes in a function and a list, and applies the function to each element of the list
+-- mapM makes more sense with its signature, Monad m => (a -> m b) -> [a] -> m [b]
+--      Basically, it takes in a function and a list, and applies the function to each element of the list
 -- mapM_ is very similar to mapM, Monad m => (a -> m b) -> [a] -> m ()
 --      It just basically ignores the results of the function (Nice when printing stuff out)
 
--- Tha '>=>' is composition of monads (Monad m => (a -> m b) -> (b -> m c) -> (a -> m c))
--- So here we compose (readFile :: FilePath -> IO String) and (putStr :: String -> IO() )
---      to get a function (FilePath -> IO) that takes in a file and prints it to standard out
+-- The '>=>' is composition of monads (Monad m => (a -> m b) -> (b -> m c) -> (a -> m c))
+-- So here we compose (readFile :: FilePath -> IO String)
+--                and (putStr   :: String   -> IO () )
+--      to get a function of type (FilePath -> IO ()) that takes in a file and prints it to standard out
 
 -- The mapM_ takes the function we composed with '>=>' and applies it to each file
 
@@ -49,3 +60,4 @@ main = getArgs >>= \files ->
    (interact id)
    (\_ -> mapM_ (readFile >=> putStr) files)
    (listToMaybe files)
+
